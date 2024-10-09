79dd6b3e274e180d20aa6af0322eaf6128c0dfd6
juan miranda <jpmiranda8183@gmail.com>
2024-10-09 07:34:47 -0300

    Agregar archivos de proyecto.

---
+++ ProyectoProgramacionMVC/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ProyectoProgramacionMVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
