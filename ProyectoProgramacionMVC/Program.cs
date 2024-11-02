8343a59bfcc8b900388dda04a1c39ee6f20bda8a
JoseAlonsoDev <alonsomvdev@gmail.com>
2024-11-02 19:20:41 -0300

    Conexion a la base de datos Edicion archivo appsettings.json

--- ProyectoProgramacionMVC/Program.cs
+++ ProyectoProgramacionMVC/Program.cs
@@ -1,7 +1,11 @@
+using Microsoft.EntityFrameworkCore;
+using ProyectoProgramacionMVC.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+builder.Services.AddDbContext<bodegaHerramientasContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("conexion")));
 
 var app = builder.Build();
 
